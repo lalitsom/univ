830daae4f4239edfd727eef994799d389a76538d
Ishan Sharma <ishan.s@juspay.in>
2024-09-15 20:23:26 +0530

    Correcting nix develop

--- backend/flake.nix
+++ backend/flake.nix
@@ -1,18 +1,35 @@
 {
-  description = "A Rust project";
+  description = "A devShell for Rust";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }: {
-    devShell = nixpkgs.lib.mkShell {
-      buildInputs = [
-          nixpkgs.rustc
-          nixpkgs.cargo
-          nixpkgs.openssl
-          nixpkgs.openssl.dev
-          nixpkgs.pkgconfig
-          nixpkgs.gcc # This includes the C compiler (cc)
-        ];
-    };
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            rust-bin.beta.latest.default
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+          '';
+        };
+      }
+    );
 }
