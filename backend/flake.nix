14e9d179eb6a138cdd8f469b28251b07b1721e9a
Ishan Sharma <ishan.s@juspay.in>
2024-09-15 23:48:58 +0530

    Adding Postgres service

--- backend/flake.nix
+++ backend/flake.nix
@@ -4,18 +4,42 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Services flake
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
+      perSystem = { self', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.rust-overlay.overlays.default ];
         };
-      in
-      {
+
+        process-compose."services" = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+            ];
+
+            services.postgres."pg1" = {
+              enable = true;
+              # initialDatabases = [
+              #   {
+              #     name = dbName;
+              #     schemas = [ "${inputs.northwind}/northwind.sql" ];
+              #   }
+              # ];
+            };
+        };
+
         devShells.default = with pkgs; mkShell {
           buildInputs = [
             openssl
@@ -23,6 +47,7 @@
             eza
             fd
             rust-bin.beta.latest.default
+            postgresql
           ];
 
           shellHook = ''
@@ -30,6 +55,33 @@
             alias find=fd
           '';
         };
-      }
-    );
+
+      };
+
+
+    };
+    #   let
+    #     overlays = [ (import rust-overlay) ];
+    #     pkgs = import nixpkgs {
+    #       inherit system overlays;
+    #     };
+    #   in
+    #   {
+    #     devShells.default = with pkgs; mkShell {
+    #       buildInputs = [
+    #         openssl
+    #         pkg-config
+    #         eza
+    #         fd
+    #         rust-bin.beta.latest.default
+    #         postgresql
+    #       ];
+
+    #       shellHook = ''
+    #         alias ls=eza
+    #         alias find=fd
+    #       '';
+    #     };
+    #   }
+    # );
 }
